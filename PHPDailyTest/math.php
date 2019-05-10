007dc34e82e2bbdd29d270b7ade6415e66f62756
748819460 <748819460@qq.com>
2019-05-10 08:46:05 +0800

    5.10

---
+++ PHPDailyTest/math.php
@@ -0,0 +1,21 @@
+<?php  
+/**
+* 
+*/
+class math
+{
+	
+	public function day1($str)
+	{
+	  $str = strlen($str);
+      for ($i=1; $i<=$str ; $i++) { 
+      	for ($j=$i[$str]; $j<=3 ; $j++) { 
+      		if($i[$str] == 3)
+      		{
+      			return $i[$str];
+      		}
+      	}
+      }
+	}
+}
+?>
