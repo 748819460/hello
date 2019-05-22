3d8842768dd90792737d2003afb927a129fe7263
748819460 <748819460@qq.com>
2019-05-22 08:40:01 +0800

    math.php

--- math.php
+++ math.php
@@ -1,15 +1,6 @@
 <?php 
-$arr = [1,2,3,4,5,6];
-$count = count($arr);
-for ($i=0; $i < $count ; $i++) { 
-	if($i%2==0)
-	{
-		echo $i;
-	}
-	if($i%2==1)
-	{
-		echo $i;
-	}
-}
+$str = "student a am I";
+$str = strret($str);
+echo $str;
 
  ?>
